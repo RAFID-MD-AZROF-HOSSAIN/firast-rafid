d48ed9d5fda686f1ba6db1c253ba11e8b9e95d48
RAFID MD AZROF HOSSAIN <129204789+RAFID-MD-AZROF-HOSSAIN@users.noreply.github.com>
2023-04-13 11:20:40 +0800

    add

---
+++ escape sequence.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<< "\t rafid hossain"<< endl;
+
+    cout<< " donia"<<endl << "01816497870"<<endl ;
+    getch();
+}
---
+++ keyword,variable data typ.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    float num3;
+    char ch;
+
+    num1=10;
+    num2=20;
+    num3=10.5;
+    ch='r';
+
+
+    cout<< "number1:- "<< num1<<endl<<num2<<endl<<num3<<endl;
+    cout<< "character: "<<ch ;
+
+
+    getch ();
+}
